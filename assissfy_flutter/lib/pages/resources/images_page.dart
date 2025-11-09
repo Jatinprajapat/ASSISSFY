8b5b3ba830c7ea3550ed81599caf0e3d544e4fb8
Jatinprajapat <jatinprajapat028@example.com>
2025-11-09 16:47:01 +0530

    Initial commit of Assissfy

---
+++ assissfy_flutter/lib/pages/resources/images_page.dart
@@ -0,0 +1,17 @@
+import 'package:assissfy_flutter/imports.dart';
+
+class ImageViewScreen extends StatelessWidget {
+  final String imagePath;
+
+  const ImageViewScreen({Key? key, required this.imagePath}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Image Viewer')),
+      body: Center(
+        child: Image.network(imagePath),
+      ),
+    );
+  }
+}
